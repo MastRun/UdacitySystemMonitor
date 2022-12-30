78833e824089dcaa30181fe44a5564d4f3565e04
MastRun <malte.staehr@gmail.com>
2022-12-30 20:01:45 +0100

    Enhanced time formatting

--- src/format.cpp
+++ src/format.cpp
@@ -13,6 +13,14 @@ string Format::ElapsedTime(long seconds) {
     int hours, minutes;
     minutes = seconds / 60;
     hours = minutes / 60;
-    string output = to_string(hours) + ":" + to_string(minutes%60) + ":" + to_string(seconds%60);
-    return output; 
+
+    string min = to_string(minutes%60);
+    min.insert(0,2-min.length(), '0');
+    string hrs = to_string(hours);
+    hrs.insert(0,2-hrs.length(), '0');
+    string sec = to_string(seconds%60);
+    sec.insert(0,2-sec.length(), '0');
+
+    //string output = to_string(hours) + ":" + to_string(minutes%60) + ":" + to_string(seconds%60);
+    return hrs + ":" + min + ":" + sec; 
 }
